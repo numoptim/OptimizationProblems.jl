bcf33fae3689d9feb37d3d58b62253985dc165aa
Vivak Patel <vp314@users.noreply.github.com>
2025-03-19 17:08:11 -0500

    template for GLM objective info tests

---
+++ test/glm/generalized_linear_model.jl
@@ -0,0 +1,19 @@
+module TestGLMWrappers
+
+using Test, OptimizationProblems, LinearAlgebra, Random 
+
+# Create struct of type GLMFamily for testing 
+# Define necessary functions 
+
+@testset "GLM Objective Information" begin
+
+    #Objective Function; Counters
+
+    #Gradient Function; Counters 
+
+    #Objective & Gradient Function; Counters 
+    
+    #Hessian Function; Counters 
+
+end
+end
